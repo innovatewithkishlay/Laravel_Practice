51b2e73077b85ad51668ed54760ecc2d2cf8acd9
Kishlay <kishlay141@gmail.com>
2026-03-06 16:40:39 +0530

    Feat: add routing like optional required miltiple routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+//This is optional parameter routing
+Route::get('/username', function ($name) {
+    return "Hello $name";
+});
+//This is required parameter routing
 Route::get('/username/{name?}', function ($name="user") {
     return "Hello $name";
+});
+//This is multiple parameter routing
+Route::get("/submitForm/{username}/{email}",function($name,$email){
+    return "Your entered name is: ". $name . " and email is: " . $email;
 });
