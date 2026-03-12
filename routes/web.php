670004175b92670b1a4bdca125b1b966c4288441
Kishlay <kishlay141@gmail.com>
2026-03-12 15:20:57 +0530

    feat: add constraint routing properly

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,15 @@ Route::get('/username', function ($name) {
 Route::get('/username/{name?}', function ($name="user") {
     return "Hello $name";
 });
-//This is multiple parameter routing
+//This is multiple parameter routing or multivalue routing
 Route::get("/submitForm/{username}/{email}",function($name,$email){
     return "Your entered name is: ". $name . " and email is: " . $email;
 });
-
+//Constraint routing is used to restrict the parameter value to a specific format.
+Route::get("/user/{id}",function($id){
+    return "Your user id is:".$id;
+ })->where("id","[0-9]{2,8}");  //here we are restricting the id to be numeric only and should be between 2 to 8 digits.
+// This will restrict the id to be numeric only.
 
 // Create a small route with parameter and return the parameter in the response. 
 // The route should be /student/{name} and the response should be "Welcome to the student page: {name}" where {name} is the parameter passed in the URL.
