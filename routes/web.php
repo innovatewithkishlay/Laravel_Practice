9b587093a712a4d391082d9411ec76bfb66f4730
Kishlay <kishlay141@gmail.com>
2026-03-12 15:43:09 +0530

    feat: did a small route actvity for constraint routing

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,18 @@ Route::get('/username/{name?}', function ($name="user") {
 Route::get("/submitForm/{username}/{email}",function($name,$email){
     return "Your entered name is: ". $name . " and email is: " . $email;
 });
-//Constraint routing is used to restrict the parameter value to a specific format.
+//Constraint routing using numbers is used to restrict the parameter value to a specific format.
 Route::get("/user/{id}",function($id){
     return "Your user id is:".$id;
  })->where("id","[0-9]{2,8}");  //here we are restricting the id to be numeric only and should be between 2 to 8 digits.
 // This will restrict the id to be numeric only.
+
+//Constraint routing using alphabets is used to restrict the parameter value to a specific format.
+Route::get("/home/{name}",function($name){
+    return "Welcom to the home page: ".$name;
+})->where("name","[A-Za-z]+");
+
+
 
 // Create a small route with parameter and return the parameter in the response. 
 // The route should be /student/{name} and the response should be "Welcome to the student page: {name}" where {name} is the parameter passed in the URL.
@@ -31,3 +38,8 @@ Route::get("/student/{name}",function($name){
 Route::get("/city/{name?}",function($name="Unknown City"){
     return "Welcome to the City: ". $name;
 });
+
+//create a route with constraint routing it should have letter k inside if not then say not found
+Route::get("/product/{name}",function($name){
+    return "Welcome to the product page: ". $name;
+})->where("name",".*k.*"); //This will restrict the name to contain the letter "k" anywhere in the string. If the name does not contain "k", it will return a 404 Not Found response.
