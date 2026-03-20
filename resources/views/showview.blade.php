08ff05ddadcaafe3d30aec371c86d80dc4c63556
innovatewithkishlay <kishlay141@gmail.com>
2026-03-20 16:40:59 +0530

    feat: add a practice question

--- resources/views/showview.blade.php
+++ resources/views/showview.blade.php
@@ -3,7 +3,23 @@
 @endphp
 
 @if($id >= $usercount)
-    <h1>User not found</h1>
+    <h1>Oooops not found</h1>
 @else
     <h1>{{ $users[$id] }}</h1>
 @endif
+
+@php
+$fact=1;
+@endphp
+@for($i=1;$i<=10;$i++)
+    @php
+    $fact=$fact*$i;
+    @endphp
+
+@endfor
+<h1>Factorial of 10 is {{$fact}}</h1>
+
+@for($i=1;$i<=$id;$i++)
+    <h3>hello</h3>
+
+@endfor
