72d9ba59ca0d09d6728d65a3dce396e7968524bb
innovatewithkishlay <kishlay141@gmail.com>
2026-04-17 15:00:02 +0530

    feat: pushing the form retrieval type

--- unit4/app/Http/Controllers/FormYZController.php
+++ unit4/app/Http/Controllers/FormYZController.php
@@ -18,22 +18,28 @@ class FormYZController extends Controller
             'phone'=>'required|numeric|min:10|max:10'
         ]);
         //Request method is used to retrieve the data from the form 
-        //one is is simple  $request->names
-        //other is via input method $request->input('name')
-        $name=$request->name;   
-        $email=$request->email;
-        $phone=$request->phone;
-        if($name==null||$email==null||$phone==null){
-            return response()->json([
-                'error'=>'All fields are required'
-            ]);
-        }
-        return response()->json([
-            'name'=>$name,
-            'email'=>$email,
-            'phone'=>$phone
-        ]);
-        // return "The name is: ".($name??"Not provided").($email??"Not provided").($phone??"Not provided");
+        //first is is simple  $request->names
+        //second is via input method $request->input('name')
+        //third is via global helper function $request->all() which will return all the data in the form of array
+        $data=$request->all();
+        // for reterieving the value from data 
+        $name=$data['name']??null;
+        $email=$data['email']??null;
+        $phone=$data['phone']??null;
+        // $name=$request->name;   
+        // $email=$request->email;
+        // $phone=$request->phone;
+        // if($name==null||$email==null||$phone==null){
+        //     return response()->json([
+        //         'error'=>'All fields are required'
+        //     ]);
+        // }
+        // return response()->json([
+        //     'name'=>$name,
+        //     'email'=>$email,
+        //     'phone'=>$phone
+        // ]);
+        return "The name is: ".($name??"Not provided").($email??"Not provided").($phone??"Not provided");
     }
 }
 
