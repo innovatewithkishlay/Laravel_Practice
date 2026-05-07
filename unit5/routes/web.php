5b1ade8502f91a228e8e1e22f94e1976c5cfd043
innovatewithkishlay <kishlay141@gmail.com>
2026-05-07 14:34:21 +0530

    Feat: add  completion of unit5

--- unit5/routes/web.php
+++ unit5/routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\ValidationYZController; //mandatory to import the contr
 //Step3: implement the logic for validation inside the custom rule class like for name field it should not be empty and should only contain letters and for email field it should not be empty and should end with @yahoo.com
 //Step4: create a controller using artisan command php artisan make:controller ValidationYZController and import the rule class inside the controller and define a method validate and inside the method use the validate method of request object to validate the input fields using the custom validation rules
 //Step5: define the route for form submission and point it to the validate method of the controller and also define the route for the form view
+// a) Few things to know like include blade error directives @error and @enderror to show the error messages for each field and also include @csrf directive inside the form to protect against cross-site request forgery attacks
+// b) also make sure to return the view with old input values using the old helper function to show the previously entered values in case of validation failure and also show the error messages for each field using the $errors variable which is available in all views by default
 Route::get('/', function () {
     return view('MyYZForm');
 });
