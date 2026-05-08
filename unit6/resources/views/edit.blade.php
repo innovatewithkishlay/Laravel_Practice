8f004bba76ed7a53cf1331f45bfe12fa2a0f42ca
innovatewithkishlay <kishlay141@gmail.com>
2026-05-08 15:12:02 +0530

    feat: add edit view

---
+++ unit6/resources/views/edit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        @csrf
+        Name: <input type="text" name="name" placeholder="Enter your name "> <br><br>
+        Email: <input type="email" name="email" placeholder="Enter your email "> <br><br>
+    <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
