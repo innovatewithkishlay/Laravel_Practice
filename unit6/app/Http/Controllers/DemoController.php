bc3bb1dd2e66a736fcbefebae9ca6820c5b97fc6
innovatewithkishlay <kishlay141@gmail.com>
2026-05-08 15:00:05 +0530

    Feat: add demo controller

--- unit6/app/Http/Controllers/DemoController.php
+++ unit6/app/Http/Controllers/DemoController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB; //mandatory to import the DB facade to use query builder
 class DemoController extends Controller
 {
     //Query builder for performing curd-model class is not used query builder is used to perform curd operations on database using query builder we can perform operations like insert,update,delete and select on database using query builder we can also perform operations like join,group by and order by on database using query builder
     public function show(){
         return view('studentform');
     }
-    public function insert(){
-
+    public function insert(Request $request){
+    $name=$request->input('name');
+    $email=$request->input('email');
+        DB::table('teachers')->insert([
+            'name'=>$name,
+            'email'=>$email,
+        ]);
+        return redirect('/read');
     }
     public function read(){
 
