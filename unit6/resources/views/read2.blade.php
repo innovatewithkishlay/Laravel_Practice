1c3ddc9db99d08f589a6d3d612cee0765a800ba5
innovatewithkishlay <kishlay141@gmail.com>
2026-05-21 15:36:36 +0530

    feat: add read2 blade

--- unit6/resources/views/read2.blade.php
+++ unit6/resources/views/read2.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Details of the Students</h2>
+    <a href="/abc/create">Add New Student</a>
+    <table border="6" cellspacing="7" style="width: 50%; margin-top: 20px;">
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+        </tr>
+        @foreach($data as $item)
+        <tr>
+            <td>{{$item->name}}</td>
+            <td>{{$item->email}}</td>
+            <td>
+                <a href="/abc/{{$data1->id}}">Show</a>
+                <a href="/abc/{{$data1->id}}/edit">Edit</a>
+                <form action="/abc/{{$data1->id}}" method="POST" style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
