e587d3922c712e8674e7bba380c01469bc082a4f
innovatewithkishlay <kishlay141@gmail.com>
2026-05-21 15:30:58 +0530

    feat: add code to show2 blade file

--- unit6/resources/views/show2.blade.php
+++ unit6/resources/views/show2.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Show details</h2>
+    <p>Name: {{$data1->name}}</p>
+    <p>Email: {{$data1->email}}</p>
+    <a href="/abc">Back</a>
+</body>
+</html>
