564e99a1e7f9018d3bc58f2514e85cec210d620e
innovatewithkishlay <kishlay141@gmail.com>
2026-05-21 15:04:05 +0530

    feat: add function to read the entries

--- unit6/app/Http/Controllers/YZController.php
+++ unit6/app/Http/Controllers/YZController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\YZ;// mandatory to import the model to use it in the controller
 class YZController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() //this is for reading the entries from the database and showing it to the user
     {
         //
+        $data=YZ::all(); //eloquent method to get all the entries from the database and store it in the variable data
+        return view('read2',compact('data'));
     }
 
     /**
