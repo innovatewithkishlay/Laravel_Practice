4b644a8c1e33b9e562dbfa460dfa6333a11ee321
innovatewithkishlay <kishlay141@gmail.com>
2026-05-21 15:24:26 +0530

    feat: add code in model

--- unit6/app/Models/YZ.php
+++ unit6/app/Models/YZ.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class YZ extends Model
 {
     //
+    protected $connection='mongodb'; //specifying the connection to be used for this model
+    protected $collection='Studentdata'; //specifying the collection to be used for this model
+    protected $fillable=['name','email']; //specifying the fillable fields for mass assignment
 }
