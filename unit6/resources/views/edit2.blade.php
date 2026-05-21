40732a506aeea075cccbf6d7e6dfdb032fdc5de7
innovatewithkishlay <kishlay141@gmail.com>
2026-05-21 15:27:25 +0530

    feat: add edit2 blade code

--- unit6/resources/views/edit2.blade.php
+++ unit6/resources/views/edit2.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Edit Student Data</h2>
+    <form action="/abc/{{$data1->id}}" method="POST">
+        @csrf
+        @method('PUT')
+        Name: <input type="text" name="name" value="{{$data1->name}}" placeholder="Enter your name "> <br><br>
+        Email: <input type="email" name="email" value="{{$data1->email}}" placeholder="Enter your email "> <br><br>
+    <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
