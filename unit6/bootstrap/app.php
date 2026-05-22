a0efffd6f1ea8b3568ace634a416eb0f68b299c2
innovatewithkishlay <kishlay141@gmail.com>
2026-05-22 15:07:22 +0530

    feat: registering the api

--- unit6/bootstrap/app.php
+++ unit6/bootstrap/app.php
@@ -8,6 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        api: __DIR__.'/../routes/api.php', //mandatory to define the route for api routes
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
