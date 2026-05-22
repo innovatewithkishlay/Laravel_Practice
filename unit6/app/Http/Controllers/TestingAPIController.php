f87e58da7b9378d164313c62c273a3e701b292a3
innovatewithkishlay <kishlay141@gmail.com>
2026-05-22 14:59:39 +0530

    feat: add code for testing Api controller and testing

--- unit6/app/Http/Controllers/TestingAPIController.php
+++ unit6/app/Http/Controllers/TestingAPIController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Testing; //importing the model to be used in the controller
 use Illuminate\Http\Request;
 
 class TestingAPIController extends Controller
 {
-    //
+    public function index()
+    {
+        return Testing::all(); //eloquent method to get all the entries from the database and return it as a response
+    }
+    public function store(Request $request)
+    {
+        Testing::create([
+            'name'=>$request->name,
+            'email'=>$request->email
+        ]); //eloquent method to create a new entry in the database with the given data from the request
+        return response()->json(['message'=>'Data created successfully']); //returning a json response with a message
+    }
 }
--- unit6/app/Models/Testing.php
+++ unit6/app/Models/Testing.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Testing extends Model
 {
-    //
+    protected $connection = 'mongodb'; //specifying the connection to be used for this model
+    protected $collection = 'Testingdata'; //specifying the collection to be used for this model
+    protected $fillable = ['name', 'email']; //specifying the fillable fields for mass assignment
 }
