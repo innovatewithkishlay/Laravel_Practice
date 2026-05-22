da9b74b887da8faf1964b5c9b260688625da9cc2
innovatewithkishlay <kishlay141@gmail.com>
2026-05-22 15:02:00 +0530

    feat: add code for update and show

--- unit6/app/Http/Controllers/TestingAPIController.php
+++ unit6/app/Http/Controllers/TestingAPIController.php
@@ -18,4 +18,17 @@ class TestingAPIController extends Controller
         ]); //eloquent method to create a new entry in the database with the given data from the request
         return response()->json(['message'=>'Data created successfully']); //returning a json response with a message
     }
+    public function show(string $id){
+        return Testing::find($id); //eloquent method to find the entry with the given id and return it as a response
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $data=Testing::find($id); //eloquent method to find the entry with the given id and store it in the variable data
+        $data->update([
+            'name'=>$request->name,
+            'email'=>$request->email
+        ]); //eloquent method to update the entry in the database with the given data from the request
+        return response()->json(['message'=>'Data updated successfully']); //returning a json response with a message
+    }
 }
